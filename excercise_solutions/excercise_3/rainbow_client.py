036b12ecb1702a8d3a24cde7d77ea003a7629e7f
Fede3751 <ftrombetti@hotmail.it>
2023-03-23 18:34:41 +0100

    Post-lecture 3 update

--- excercise_solutions/excercise_3/rainbow_client.py
+++ excercise_solutions/excercise_3/rainbow_client.py
@@ -3,6 +3,9 @@ from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
+from turtlesim.srv import SetPen
+
+from random import randint
 
 
 turtle_moves = [
@@ -22,16 +25,27 @@ class TurtlePublisher(Node):
             '/turtle1/cmd_vel',
             10
         )
+        self.client = self.create_client(SetPen, '/turtle1/set_pen')
+        self.req = SetPen.Request()
 
         self.timer = self.create_timer(1, self.timer_callback)
-        self.i = 0
+        self.i = 0  
 
     def timer_callback(self):
         msg = Twist()
         msg.linear = turtle_moves[self.i%4]
         msg.angular = Vector3(x=0.0, y=0.0, z=0.0)
-        self.i += 1
+
+        self.req.r = randint(0, 255)
+        self.req.g = randint(0, 255)
+        self.req.b = randint(0, 255)
+        self.req.width = 10
+        self.req.off = 0
+
+        self.future = self.client.call_async(self.req)
         self.publisher.publish(msg)
+
+        self.i += 1
 
 
 def main(args=None):
